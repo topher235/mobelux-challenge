8695a6a556f0ae71d0617138c80a6d2e5da1fc93
topher235 <topherhurt@gmail.com>
2021-06-25 12:09:35 -0400

    Adds feature for user to create an album

--- flickr_clone/flickr_clone/urls.py
+++ flickr_clone/flickr_clone/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('auth.urls')),
+    path('flickr/', include('flickrapp.urls')),
 ]
