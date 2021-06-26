dc008b2a77b5f60d39f32657a1c95040bb1f4229
topher235 <topherhurt@gmail.com>
2021-06-26 13:09:59 -0400

    Adds some navigation quality of life improvements

--- flickr_clone/auth/urls.py
+++ flickr_clone/auth/urls.py
@@ -1,10 +1,12 @@
 from django.contrib.auth import views as auth_views
+from django.shortcuts import redirect
 from django.urls import path
 
 from .views import ProfileView
 
 
 urlpatterns = [
+    path('', lambda request: redirect('profile/', permanent=True)),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('profile/', ProfileView.as_view(), name='profile'),
 ]
